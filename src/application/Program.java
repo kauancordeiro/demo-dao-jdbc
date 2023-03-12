aaee1a93b0b60e732ac9a192a0dd3abe0ca11bc4
kauancordeiro <cvitorcordeiro@gmail.com>
2023-03-12 20:59:37 -0300

    criado a entidade vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.Date;
 import java.util.Locale;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -11,8 +13,10 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
+		Seller seller = new Seller(1, "Jhonas", "jhonas@gmail.com", new Date(), 3000.0, obj);
 		
-		System.out.println(obj);
+		
+		System.out.println(seller);
 	}
 
 }
