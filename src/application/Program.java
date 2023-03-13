d066f6e7354be22a875d57c527ebd09729d0c94b
kauancordeiro <cvitorcordeiro@gmail.com>
2023-03-13 20:18:41 -0300

    implementando update

--- src/application/Program.java
+++ src/application/Program.java
@@ -53,6 +53,14 @@ public class Program {
 		sellerDao.insert(newSeller);
 		
 		System.out.println("INserted! new ID = " + newSeller.getId());
+		
+		
+		System.out.println("\n === TEST 5: seller insert===");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
 	
