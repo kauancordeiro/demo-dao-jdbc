8eb15a260bbc62ca4e75f2fccdc169e01f8da744
kauancordeiro <cvitorcordeiro@gmail.com>
2023-03-12 21:31:24 -0300

    Implementando findByID

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,12 +13,13 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		
-		Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(1, "Jhonas", "jhonas@gmail.com", new Date(), 3000.0, obj);
 		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
